ade680e3aa3ca5a9a21e6aca77b0bc2db165b90a
ChinaKamal1 <61737049+ChinaKamal1@users.noreply.github.com>
2020-05-03 17:04:55 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -145,10 +145,10 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"__My Master Has Been Gone For__ `{total_afk_time}`\nWhere He Is: ~~ONLY GOD KNOWS~~ " + \
-            f"\n\n__I promise I'll back in after CORONA leaves.__\n**REASON**: {reason}" \
+        message_to_reply = f"__Am being offline from__ `{total_afk_time}`\nWhere He Is: ~~Enjoying With some Ladies In Tub~~ " + \
+            f"\n\n__I promise I'll back suddenly after CORONA leaves.__\n**REASON**: {reason}" \
             if reason \
-            else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? ~~Soon~~ __Whenever I feel like it__**( ಠ ʖ̯ ಠ)**  "
+            else f"**YOU NUB**\n__I am offline now. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? ~~Soon~~ __When Corona leaves**(☣)**  "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
